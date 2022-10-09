8696614b19cf55eaef500b958977d6ac39591463
Roberto <47633569+Ptts2@users.noreply.github.com>
2022-10-09 12:14:24 +0200

    errors, warnings and rules broken flagged

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 ## ejemplo1.c
 
-Follow the instructions explained in the beginning of file ejemplo1.c for compiling with gcc and g++ (you can try also clang). 
+Follow the instructions explained in the beginning of file ejemplo1.c for compiling with gcc and g++ (you can try also clang).
+
+SOLUTION:
+
+In the base file of ejemplo1 the rules and recomendations are pointed and a solution is proposed
+then in the files ejemplo1g++.c and ejemplo1gcc99.c fixes for g++ and gcc -std=c99 are proposed
 
 ## ejemplo2.c. Program Instrumentation
 
--- ejemplo1.c
+++ ejemplo1.c
@@ -10,6 +10,25 @@
  * There are several variants. You should choose at least two. You can also try clang.
  * At the end the source code  should compile without warnings to the variant 
  *     selected (you can remove/change instructions).
+ * 
+ * ---------------------------------------------------------------------------
+ * Broken rules (explained in the document):
+ * 
+ * in gcc -std=c99 and -std=c11
+ * 
+ * returning a value in a function that is supposed to return void
+ * 
+ * in g++ 
+ * 
+ * returning a value in a function that is supposed to return void
+ * ISO C++ forbids converting a string constant to 'char*'
+ * 
+ * 
+
+ * 
+ * 
+ * ---------------------------------------------------------------------------
+ * Variants choosen for fixing the code: gcc -std=c99 -Wall and g++ -Wall
  */
 
 #include <stdio.h>
@@ -20,12 +39,47 @@ char array1[] = "Foo" "bar";
 char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
  
 enum { BUFFER_MAX_SIZE = 1024 };
- 
+
+
+/*
+* COMPILATION WARNING AND ERRORS
+* ------------------------------
+* using -std=c99 and -std=c11 (the compilation gives the same result)
+* warning: missing terminating " character const char* s1 = R"foo(
+* error: missing terminating " character const char* s1 = R"foo(
+* error: 'R' undeclared here (not in a function) const char* s1 = R"foo(
+* error: expected ',' or ';' before 'Hello'
+* warning: missing terminating " character )foo";
+* error: missing terminating " character )foo";
+* 
+* R IS NOT A VALID SYNTAX FOR C
+*
+* This does correctly compile using g++ because R is a valid syntax using C++
+*
+* There are not broken recomendations or rules in this code
+*
+*/
 const char* s1 = R"foo(
 Hello
 World
 )foo";
+
+/* in gcc std c99 and c11 error: 's2' undeclared. Because the previous instruction is not valid */
 const char* s2 = "\nHello\nWorld\n";
+
+
+
+/*
+* COMPILATION WARNING AND ERRORS
+* ------------------------------
+* Using -std=c99 and -std=c11 (the compilation gives the same result) warning
+* warning: 'return' with a value, in function returning void return 1;
+*
+* Using g++ error
+* 
+* error: return-statement with a value, in function returning 'void' [-fpermissive] return 1;
+* 
+*/
 
 void gets_example_func(void) {
   char buf[BUFFER_MAX_SIZE];
@@ -36,16 +90,29 @@ void gets_example_func(void) {
   buf[strlen(buf) - 1] = '\0';
 }
 
+/*
+* BROKEN RULES AND RECOMENDATIONS
+* STR30-C. Do not attempt to modify string literals
+* since the argument of strrchr points to a string literal the behavior is undefined
+* 
+* A possible solution could be avoiding to modify a string literal and using a char array instead
+*/
+
 const char *get_dirname(const char *pathname) {
   char *slash;
   slash = strrchr(pathname, '/');
   if (slash) {
-    *slash = '\0'; /* Undefined behavior */
+    *slash = '\0'; /*Undefined behavior*/
   }
   return pathname;
 }
- 
 
+
+/*
+* BROKEN RULES AND RECOMENDATIONS
+* MSC24-C. Do not use deprecated or obsolescent functions
+* gets is an obsolescent function and should not be used, instead fgets or gets_s should be used 
+*/
 void get_y_or_n(void) {  
 	char response[8];
 
@@ -58,7 +125,22 @@ void get_y_or_n(void) {
 	return;
 }
 
- 
+ /*
+ * COMPILATION WARNING AND ERRORS
+ * ------------------------------
+ * Using gcc -std=c99 and -std=c11
+ * 
+ * error: 's2' undeclared (first use in this function); did you mean 's1'? puts (s2);
+ * 
+ * Using g++
+ * warning: ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings] char *ptr_char  = "new string literal";
+ * 
+ * Warning of unused variables flagged with -Wall in both gcc and g++
+ * analitic3
+ * size_array2
+ * size_array1
+ * 
+ */
 int main(int argc, char *argv[])
 {
     char key[24];
@@ -72,11 +154,26 @@ int main(int argc, char *argv[])
     
    // char analitic1[size_array1]="аналитик";
    // char analitic2[size_array2]="аналитик";
+
+   /*
+    * BROKEN RULES AND RECOMENDATIONS
+    * STR11-C. Do not specify the bound of a character array initialized with a string literal
+    * solution = char analitic3[]="аналитик";
+    */
     char analitic3[100]="аналитик";
 
     puts(get_dirname(__FILE__));
 
-        
+    /* BROKEN RULES AND RECOMENDATIONS
+    * STR31-C. Guarantee that storage for strings has sufficient space for character data and the null terminator
+    * Since argv has an undefined size, the size of the array key might not be large enough to hold the string
+    * A posible solution could be using strncpy_s instead of strcpy
+    * 
+    * 
+    * MSC24-C. Do not use deprecated or obsolescent functions 
+    * strcpy is an obsolescent function and should not be used, instead strcpy_s should be used
+    * strcat is an obsolescent function and should not be used, instead strcat_s should be used
+    */   
     strcpy(key, argv[1]);  
     strcat(key, " = ");  
     strcat(key, argv[2]);
@@ -96,10 +193,20 @@ int main(int argc, char *argv[])
     puts (s2);
     printf ("\n");
     
+    /*
+    * MSC24-C. Do not use deprecated or obsolescent functions 
+    * strncpy is an obsolescent function and should not be used, instead strncpy_s should be used
+    * 
+    */  
     strncpy(array3, array5, sizeof(array3));  
     strncpy(array4, array3, strlen(array3));
     
     array5 [0] = 'M';
+
+    /* BROKEN RULES AND RECOMENDATIONS
+    * STR30-C. Do not attempt to modify string literals 
+    * using a char array instead of a string literal
+    */
     ptr_char [0] = 'N';
     
     array3[sizeof(array3)-1]='\0';
@@ -107,3 +214,4 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+
